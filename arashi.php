319ac3ead3e3aedc775d5b0e2558bbd9a82a19b5
arasHi <arasi27676271@gmail.com>
2019-03-07 00:21:21 +0800

    text

--- arashi.php
+++ arashi.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * Copyright 2017 GoneTone
- *
- * Line Bot
- * 範例 Example Bot 執行主文件
- *
- * 此範例 GitHub 專案：https://github.com/GoneTone/line-example-bot-php
- * 官方文檔：https://devdocs.line.me/en/
- */
-error_reporting(0); // 不顯示錯誤 (Debug 時請註解掉)
+// error_reporting(0); // 不顯示錯誤 (Debug 時請註解掉)
 date_default_timezone_set("Asia/Taipei"); // 設定時區為台北時區
 
 require_once('LINEBotTiny.php');
@@ -43,23 +34,29 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    require_once('includes/text.php'); // Type: Text
-                    require_once('includes/image.php'); // Type: Image
-                    require_once('includes/video.php'); // Type: Video
-                    require_once('includes/audio.php'); // Type: Audio
-                    require_once('includes/location.php'); // Type: Location
-                    require_once('includes/sticker.php'); // Type: Sticker
-                    require_once('includes/imagemap.php'); // Type: Imagemap
-                    require_once('includes/template.php'); // Type: Template
+                    if (strtolower($message['text']) == 'text') {
+                        $client->replyMessage([
+                            'replyToken' => $event['replyToken'],
+                            'messages'   => [
+                                [
+                                    'type' => 'text',
+                                    'text' => 'Hello world!'
+                                ]
+                            ]
+                        ]);
+                    }
                     break;
+
                 default:
                     //error_log("Unsupporeted message type: " . $message['type']);
                     break;
             }
             break;
+        
         case 'postback':
             //require_once('postback.php'); // postback
             break;
+        
         default:
             //error_log("Unsupporeted event type: " . $event['type']);
             $client->replyMessage(array(
